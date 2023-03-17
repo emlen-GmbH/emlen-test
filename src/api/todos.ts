ad78175667c06a190437994b9267870c1f63ba84
Leo <leonard.hassler@emlen.io>
2023-03-17 09:58:03 +0100

    test

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,28 +2,32 @@ import type { ITodos } from '@/types/todo'
 
 export default {
     getTodos,
-    setTodos
+    saveTodos
 }
 
 export const LOCAL_STORAGE_KEY = 'todos'
 
-function getTodos(): ITodos {
+async function getTodos(): Promise<ITodos> {
     const savedTodos = localStorage.getItem(LOCAL_STORAGE_KEY)
 
-    return savedTodos ? JSON.parse(savedTodos) : [
+    const todos = savedTodos ? JSON.parse(savedTodos) : [
         {
             icon: 'calendar',
-            iconColor: 'blue',
-            description: 'Parmesan airedale dolcelatte. Bocconcini camembert de normandie cheese and wine when the cheese comes out everybody\'s happy fondue lancashire feta rubber cheese. Cheese strings jarlsberg feta cut the cheese manchego cheddar babybel pepper jack. Stilton dolcelatte danish fontina cauliflower cheese.'
+            description: 'Parmesan airedale dolcelatte. Bocconcini camembert de normandie cheese and wine when the cheese comes out everybody\'s happy fondue'
         },
         {
             icon: 'file',
-            iconColor: 'green',
             description: 'Parmesan airedale dolcelatte. Bocconcini camembert de normandie cheese and wine when the cheese comes out everybody\'s happy fondue lancashire feta rubber cheese. Cheese strings jarlsberg feta cut the cheese manchego cheddar babybel pepper jack. Stilton dolcelatte danish fontina cauliflower cheese.'
         },
+        {
+            icon: 'users',
+            description: 'Parmesan airedale dolcelatte.'
+        },
     ]
+
+    return new Promise(resolve => setTimeout(() => resolve(todos), 100))
 }
 
-function setTodos(todos: ITodos) {
+async function saveTodos(todos: ITodos): Promise<void> {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
 }
